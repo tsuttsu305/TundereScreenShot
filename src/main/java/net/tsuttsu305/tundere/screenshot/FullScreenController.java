b836931f38b53bbe716661c00142f9c5a6f27921
tsuttsu305 <tundere@tsuttsu305.net>
2014-06-28 23:26:36 +0900

    Add Comment

--- src/main/java/net/tsuttsu305/tundere/screenshot/FullScreenController.java
+++ src/main/java/net/tsuttsu305/tundere/screenshot/FullScreenController.java
@@ -32,11 +32,13 @@ public class FullScreenController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        //GUIのパネルを半透明に設定(白+アルファ値)
         pane.setStyle("-fx-background-color: rgba(255,255,255,0.01);");
 
+        //選択枠表示四角形描画用インスタンス登録
         pane.getChildren().add(rec);
 
-        /*D&D中*/
+        //D&D中の処理を設定
         pane.setOnMouseDragged(new EventHandler<MouseEvent>() {
             double xt, yt;
 
@@ -66,7 +68,7 @@ public class FullScreenController implements Initializable {
             }
         });
 
-        /*クリックされた*/
+        //クリックの動作を設定
         pane.setOnMousePressed(event -> {
             if (event.getButton() != MouseButton.PRIMARY) return;
 
@@ -81,7 +83,7 @@ public class FullScreenController implements Initializable {
             System.out.println("D&D Enter: " + event.getX() + ", " + event.getY());
         });
 
-        /*クリックはなされた*/
+        //クリックが終わった時の動作を設定
         pane.setOnMouseReleased(event -> {
             if (event.getButton() != MouseButton.PRIMARY) return;
 
@@ -90,23 +92,30 @@ public class FullScreenController implements Initializable {
 
             if (sx != ex && sy != ey) {
                 try {
+                    //画面を撮影するため、ウィンドウは非表示にする
                     stage.hide();
+
+                    //デスクトップ撮影用処理
                     Robot robot = new Robot();
                     java.awt.Rectangle size = new java.awt.Rectangle((int) sx, (int) sy, (int) ex - (int) sx, (int) ey - (int) sy);
                     BufferedImage bufferedImage = robot.createScreenCapture(size);
                     File f = File.createTempFile("ScreenShot", ".png");
                     ImageIO.write(bufferedImage, "png", f);
 
+                    //Fileをクリップボードにコピーするための処理
+                    //Systemのクリップボードを取得
                     Clipboard cp = Clipboard.getSystemClipboard();
                     ClipboardContent cc = new ClipboardContent();
                     ArrayList<File> af = new ArrayList<>();
                     af.add(f);
+                    //クリップボードContentにFileリストを登録
                     cc.putFiles(af);
+                    //クリップボードに反映
                     cp.setContent(cc);
                 } catch (Exception e) {
                     e.printStackTrace();
                 } finally {
-                    //stage.show();
+                    //プログラムを終了する
                     stage.close();
                 }
             }
--- src/main/java/net/tsuttsu305/tundere/screenshot/TundereScreenShot.java
+++ src/main/java/net/tsuttsu305/tundere/screenshot/TundereScreenShot.java
@@ -22,28 +22,37 @@ public class TundereScreenShot extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        //FXMLのロード先を指定してロード用インスタンスを作成
         FXMLLoader loader = new FXMLLoader(this.getClass().getResource("FullScreen.fxml"));
 
+        //FXMLロード
         Parent parent = loader.load();
 
+        //コントローラのインスタンスを取得しStageをsetする
         FullScreenController controller = loader.getController();
         controller.setStage(primaryStage);
 
+        //Scene作成
         Scene scene = new Scene(parent);
+        //塗りつぶしなし
         scene.setFill(null);
+        //マウスカーソルは十字に
         scene.setCursor(Cursor.CROSSHAIR);
+        //Escキーが押されたらプログラムを終了するように設定
         scene.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
             if (event.getCode() == KeyCode.ESCAPE) {
                 primaryStage.close();
             }
         });
 
+        //ウィンドウ枠を透明に設定
         primaryStage.initStyle(StageStyle.TRANSPARENT);
         primaryStage.setScene(scene);
         primaryStage.setTitle("TundereScreenShot");
         primaryStage.setFullScreen(true);
         primaryStage.setFullScreenExitKeyCombination(KeyCombination.keyCombination("Esc"));
         primaryStage.setFullScreenExitHint("Escでキャプチャを中止");
+        //ウィンドウを表示
         primaryStage.show();
     }
 }
